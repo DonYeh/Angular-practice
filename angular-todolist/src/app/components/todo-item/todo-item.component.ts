9a006e53eeb363e6140e8d88f5683401b8e9ded7
Donald Yeh <Donald.Yeh@gmail.com>
2019-07-27 23:43:26 -0400

    adds todo-item input checkbox and delete buttons

--- angular-todolist/src/app/components/todo-item/todo-item.component.ts
+++ angular-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -12,4 +12,13 @@ export class TodoItemComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      "is-completed": this.todo.completed //because it's hyphenated, have to use quotation marks ''
+    };
+
+    return classes;
+  }
 }
